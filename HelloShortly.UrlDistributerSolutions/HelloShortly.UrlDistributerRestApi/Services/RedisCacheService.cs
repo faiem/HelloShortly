ac8601c0ef0212aa12ac30540954d612e8d1c416
faiem <faiem@overseasecurity.com>
2021-06-07 06:26:21 -0700

    - Added Redis Cache Services and distributed cache storage between 16 nodes.

---
+++ HelloShortly.UrlDistributerSolutions/HelloShortly.UrlDistributerRestApi/Services/RedisCacheService.cs
@@ -0,0 +1,48 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloShortly.UrlDistributerRestApi.Services
+{
+    public class RedisCacheService : ICacheService
+    {
+        private readonly IConnectionMultiplexer _connectionMultiplexer;
+
+        public RedisCacheService(IConnectionMultiplexer connectionMultiplexer)
+        {
+            _connectionMultiplexer = connectionMultiplexer;
+        }
+
+        public async Task<string> GetCacheValueAsync(string key)
+        {
+            if (key == null)
+                return null;
+            int charSum = 0;
+            foreach (char c in key)
+            {
+                charSum += (int)c;
+            }
+            int index = charSum % 16;
+            var db = _connectionMultiplexer.GetDatabase(index);
+            return await db.StringGetAsync(key);
+        }
+
+        public async Task SetCacheValueAsync(string key, string value)
+        {
+            if (key != null)
+            {
+                int charSum = 0;
+                foreach (char c in key)
+                {
+                    charSum += (int)c;
+                }
+                int index = charSum % 16;
+                var db = _connectionMultiplexer.GetDatabase(index);
+                await db.StringSetAsync(key, value);
+                await db.KeyExpireAsync(key, DateTime.UtcNow.AddDays(7));
+            }
+        }
+    }
+}
