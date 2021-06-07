34a3a8053944945eb84f4fc048431341497d2736
faiem <faiem@overseasecurity.com>
2021-06-07 05:29:52 -0700

    - Added Base62 converter from a number to generate unique aliases for short url

---
+++ HelloShortly.UrlDistributerSolutions/HelloShortly.UrlDistributerRestApi/ShortAliasesHelper/Base62Generator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloShortly.UrlDistributerRestApi.ShortAliasesHelper
+{
+    public class Base62Generator
+    {
+        private readonly string keyElements = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private readonly long Base = 62;
+
+        public long Decode(string value)
+        {
+            long i = 0;
+
+            foreach (var c in value)
+            {
+                i = (i * Base) + keyElements.IndexOf(c);
+            }
+
+            return i;
+        }
+
+        public string Encode(long value)
+        {
+            if (value == 0) return keyElements[0].ToString();
+
+            var s = string.Empty;
+
+            while (value > 0)
+            {
+                int x = (int)(value % Base);
+                s += keyElements[x];
+                value /= Base;
+            }
+            return string.Join(string.Empty, s.Reverse());
+        }
+    }
+}
---
+++ HelloShortly.UrlDistributerSolutions/HelloShortly.UrlDistributerRestApi/ShortAliasesHelper/IBase62Generator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloShortly.UrlDistributerRestApi.ShortAliasesHelper
+{
+    public interface IBase62Generator
+    {
+        string Encode(long value);
+        long Decode(string value);
+    }
+}
