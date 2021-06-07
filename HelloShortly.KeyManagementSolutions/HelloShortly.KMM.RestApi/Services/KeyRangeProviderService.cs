2b78d3e350f5a19448cd61f17c93b1a5b0500cc2
faiem <faiem@overseasecurity.com>
2021-06-07 01:23:01 -0700

    - Implemented KeyRangeProviderService to fetch unique key ranges

---
+++ HelloShortly.KeyManagementSolutions/HelloShortly.KMM.RestApi/Services/KeyRangeProviderService.cs
@@ -0,0 +1,39 @@
+﻿using HelloShortly.KMM.RestApi.Data.Repositories;
+using HelloShortly.KMM.RestApi.Dtos.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloShortly.KMM.RestApi.Services
+{
+    public class KeyRangeProviderService : IKeyRangeProviderService
+    {
+        private readonly IKeyRangeRepository _keyRangeRepository;
+        public KeyRangeProviderService(IKeyRangeRepository keyRangeRepository)
+        {
+            _keyRangeRepository = keyRangeRepository;
+        }
+        public KeyRangeResponseDto GetUniqueKeyRange()
+        {
+            string retrieveKeyIdentifier = Guid.NewGuid().ToString();
+            int rowUpdated = _keyRangeRepository.UpdateKeyRangeWithUniqueIdentifier(retrieveKeyIdentifier);
+
+            var key_range_Response = new KeyRangeResponseDto(-1, 0, 0);
+
+            if (rowUpdated == 1)
+            {
+                var keyRange = _keyRangeRepository.GetUniqueKeyRangeByIdentifier(retrieveKeyIdentifier);
+                if (keyRange != null)
+                {
+                    key_range_Response.range_id = keyRange.key_ranges_id;
+                    key_range_Response.start_range = keyRange.start_range;
+                    key_range_Response.end_range = keyRange.end_range;
+                }
+            }
+
+            return key_range_Response;
+        }
+
+    }
+}
