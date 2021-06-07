22e8c6f367e77b3cf11a3da2ff01a5f60137ef1e
faiem <faiem@overseasecurity.com>
2021-06-07 05:01:53 -0700

    - Added new Micro-service app named "HelloShortly.UrlDistributerRestApi" - Configured swagger - Configured Dockerfile - Configured Logger with Seq

---
+++ HelloShortly.UrlDistributerSolutions/HelloShortly.UrlDistributerRestApi/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloShortly.UrlDistributerRestApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Seq(Environment.GetEnvironmentVariable("ASPNETCORE_LOG_HOST"))
+                .CreateLogger();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                }).UseSerilog();
+    }
+}
